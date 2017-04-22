e59f7371b0b7f85db7111167545940910d8a7cc8
Blake Larson <blake7@hawaii.edu>
2017-04-21 15:30:36 -1000

    rebuilding site Fri Apr 21 15:30:36 HST 2017

--- bookshelf/deploy.sh
+++ bookshelf/deploy.sh
@@ -2,11 +2,16 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+rm -rf public/*
+
 # Build the project.
-hugo # if using a theme, replace by `hugo -t <yourtheme>`
+hugo -t hugo-creative-theme
 
 # Go To Public folder
 cd public
+
+mv * ~/Documents/GitRepos/deployed-hugo-website/
+
 # Add changes to git.
 git add -A
 
@@ -15,10 +20,14 @@ msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+
 git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
 
-# Come Back
-cd ..
+cd ~/Documents/GitRepos/deployed-hugo-website/
+git add .
+git commit -m "$msg"
+git push origin master
+cd ~/Documents/GitRepos/website-Hugo/bookshelf/
